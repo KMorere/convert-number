68880dec95b59c154265434adf3f676e9092c350
Killian Morere <killian.morere@fms-ea.com>
2025-11-03 08:09:28 +0100

    Added convertion until 100k.

--- src/def/NumberToLetter_fr.java
+++ src/def/NumberToLetter_fr.java
@@ -2,10 +2,9 @@ package def;
 
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Collections;
 
 public class NumberToLetter_fr {
 	
@@ -42,11 +41,13 @@ public class NumberToLetter_fr {
 			"Quatre-vingt",
 			"Quatre-vingt-dix",
 	};
+	private static String[] thousands = { "", "Mille", "Million", "Milliard" };
 	
 	private static String[][] numbers = {
 			ones,
 			tenToTwenty,
 			tens,
+			thousands
 	};
 	
 	private static String[] special = {
@@ -54,12 +55,12 @@ public class NumberToLetter_fr {
 			"Quatre-vingt-dix"
 	};
 	
-	private static final int MAX_VALUE = 999;
+	private static final int MAX_VALUE = 99999;
 	private static final int MIN_VALUE = 1;
 	
 	public static void main(String[] args) {
 		System.out.printf("Entrez un nombre entre %d et %d, "
-				+ "la valeur décimale étant séparé par ',' : "
+				+ "la valeur décimale étant séparée par ',' : \n"
 				, (MIN_VALUE-1), (MAX_VALUE+1));
 		
 		tryConvert();
@@ -72,7 +73,7 @@ public class NumberToLetter_fr {
 		if (scan.hasNextFloat()) {
 			float input = scan.nextFloat();
 			
-			if (input >= 0 && input < 1000) {
+			if (input >= MIN_VALUE && input < MAX_VALUE) {
 				String result = (input % 1 == 0) ? String.valueOf((int) input) : String.valueOf(input);
 				String[] inputStr = result.split("");
 				
@@ -115,14 +116,20 @@ public class NumberToLetter_fr {
 		}
 		
 		String result = "";
-		System.out.print(convertNumber(num.toArray(new String[num.size()])));
+		result += (convertNumber(num.toArray(new String[num.size()])));
 		
 		if (isDecimal) {
-			System.out.print("virgule ");
-			System.out.println(convertNumber(decimals.toArray(new String[decimals.size()])));
+			result += ("virgule ");
+			result += (convertNumber(decimals.toArray(new String[decimals.size()])));
 		}
 		
-		System.out.println(result);
+		result = result.trim().toLowerCase();
+		
+		System.out.println(result.substring(0, 1).toUpperCase() + result.substring(1));
+	}
+	
+	private static float splitNumber(String[] number) {
+		return (float)number.length/3;
 	}
 	
 	/**
@@ -130,20 +137,79 @@ public class NumberToLetter_fr {
 	 * @param number The number to convert with each digit separated.
 	 */
 	private static String convertNumber(String[] number) {
+		ArrayList<String> numbers = new ArrayList<String>();
+		
+		String temp = "";
+		float size = splitNumber(number);
+		String[] numSep = new String[((int)size+1) * 3];
+		
+		for (int i = 0; i < size; i++) {
+			if (size > 1) { // If the number is above 999 separate every 3 digits.
+				for (int n = 0; n < numSep.length; n++) {
+					if (numSep.length - number.length > 0) {
+						if (n < numSep.length - number.length)
+							numSep[n] = "0";
+						else
+							numSep[n] = number[n - (numSep.length - number.length)];
+					}
+					else if (numSep.length - number.length == 0)
+						numSep[n] = number[n];
+				}
+				
+				String[] num = setArray(numSep, i);
+
+				numbers.add(setNumber(num));
+				
+				numbers.add(thousands[(int)size-i] + " ");
+				
+				if (size <= 1) {
+					numbers.add(setNumber(setArray(numSep, 0)));
+				}
+			}
+			else if (size <= 1) {
+				numbers.add(setNumber(number));
+			}
+		}
+		
+		for (int i = numbers.size()-1; i >= 0; i--)
+			if (!numbers.get(i).equals(" ") && (!numbers.get(i).trim().equals(ones[0])))
+				temp += numbers.get(i);
+
+		return temp;
+	
+	}
+	
+	// Separate the number every 3 digits.
+	private static String[] setArray(String[] number, int index) {
+		int ind = 3;
+		for (int i = 0; i < 2; i++) {
+			if (number[i + (number.length - 3 * ((int)splitNumber(number)-1 + index))] != "0") break;
+			else ind--;
+		}
+		
+		String[] num = new String[ind];
+		
+		for (int n = 0; n < ind; n++) {
+			num[ind-1-n] = number[number.length - 1 - n - (3 * index)];
+		}
+		
+		return num;
+	}
+	
+	// Convert digits in 3 positions.
+	private static String setNumber(String[] number) {
 		String temp = "";
 		
-		for (int i = 0; i < 1; i++) {
-			switch (number.length - i) {
-				case 3: // Current number is in hundreds.
-					temp += setHundreds(number, i);
-					break;
-				case 2: // 2nd position is in tens.
-					temp += setTens(number, i);
-					break;
-				case 1: // 3rd digit with numbers from 1 to 9.
-					temp += setOnes(number, i);
-					break;
-			}
+		switch (number.length) {
+			case 3: // Current number is in hundreds.
+				temp += setHundreds(number, 0);
+				break;
+			case 2: // 2nd position is in tens.
+				temp += setTens(number, 0);
+				break;
+			case 1: // 3rd digit with numbers from 1 to 9.
+				temp += setOnes(number, 0);
+				break;
 		}
 
 		return temp;
